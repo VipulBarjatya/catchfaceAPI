77c9813cf2441e5628048b9f1b44a8e3b98a02d4
Vipul <vipulbarjatya@gmail.com>
2022-11-08 23:50:59 +0530

    All set ready to host

--- server.js
+++ server.js
@@ -2,21 +2,59 @@ const express = require("express");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
-const Clarifai = require("clarifai");
+// const Clarifai = require("clarifai");
 const { json } = require("express");
 const app = express();
 
-const api = new Clarifai.App({
-  apiKey: "396f265e809b41d2bfb2fb8a744a989f",
-});
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key 6c19a20eb81e4d6ba8876ab597781e2c");
+
+// const api = new Clarifai.App({
+//   apiKey: "396f265e809b41d2bfb2fb8a744a989f",
+// });
 
 const handleApiCall = (req, res) => {
-  api.models
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => res.status(400).json("unable to work with API"));
+  stub.PostModelOutputs(
+    {
+      // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+      model_id: "a403429f2ddf4b49b307e318f00e528b",
+      inputs: [{ data: { image: { url: req.body.input } } }],
+    },
+    metadata,
+    (err, response) => {
+      if (err) {
+        console.log("Error: " + err);
+        return;
+      }
+
+      if (response.status.code !== 10000) {
+        console.log(
+          "Received failed status: " +
+            response.status.description +
+            "\n" +
+            response.status.details
+        );
+        return;
+      }
+
+      console.log("Predicted concepts, with confidence values:");
+      for (const c of response.outputs[0].data.concepts) {
+        console.log(c.name + ": " + c.value);
+      }
+      res.json(response);
+    }
+  );
+
+  // api.models
+  //   .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  //   .then((data) => {
+  //     res.json(data);
+  //   })
+  //   .catch((err) => res.status(400).json("unable to work with API"));
 };
 
 const db = knex({
