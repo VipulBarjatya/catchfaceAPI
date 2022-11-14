0a3ea3b31cea68a8631858755f375a20a01e4949
Vipul <vipulbarjatya@gmail.com>
2022-11-14 10:55:30 +0530

    process env

--- server.js
+++ server.js
@@ -165,6 +165,6 @@ app.post("/imageurl", (req, res) => {
   handleApiCall(req, res);
 });
 
-app.listen(3001, () => {
-  console.log("server is running on port 3001");
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
 });
